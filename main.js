612bafe861ebea78c46ba6087a0acbe79cedc9a9
Henry Stroud <henry.stroud@hotmail.co.uk>
2019-05-16 16:34:05 +0100

    added safari compatability

--- main.js
+++ main.js
@@ -489,7 +489,7 @@ window.addEventListener('DOMContentLoaded', () => {
       this.classRemovedCallback = classRemovedCallback
       this.observer = null
       this.lastClassState = targetNode.classList.contains(this.classToWatch)
-
+      this.mutationCallback = this.mutationCallback.bind(this)
       this.init()
     }
 
@@ -504,23 +504,27 @@ window.addEventListener('DOMContentLoaded', () => {
 
     disconnect() {
       this.observer.disconnect()
+
     }
 
-    mutationCallback = mutationsList => {
+    mutationCallback(mutationsList) {
       for(const mutation of mutationsList) {
         if (mutation.type === 'attributes' && mutation.attributeName === 'class') {
           const currentClassState = mutation.target.classList.contains(this.classToWatch)
           if(this.lastClassState !== currentClassState) {
             this.lastClassState = currentClassState
             if(currentClassState) {
+              console.log(this)
               this.classAddedCallback(this)
             } else {
+              console.log(this)
               this.classRemovedCallback(this)
             }
           }
         }
       }
     }
+
   }
 
   new ClassWatcher(youBetButton, 'active', workOnClassAdd, workOnClassRemoval)
